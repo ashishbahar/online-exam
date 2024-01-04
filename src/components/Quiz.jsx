4d3e9f73a6706e8d3811aac7e0b34d156f3b0b05
ashishbahar <ashishbahar987@gmail.com>
2024-01-04 13:15:54 +0530

    clone code

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Quiz = () => {
-  return <div>Quiz</div>;
+  return (
+    <>
+      <div>
+        <h2 className=" bg-black ">QUIZ TIME</h2>
+      </div>
+    </>
+  );
 };
 
 export default Quiz;
