10966f187e8b60b9aa11de01bd679971ca26f85d
gauravsaini98966 <gs9042312@gmail.com>
2024-01-10 12:40:19 +0530

    new

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import "./App.css";
 // import Quiz from "./components/Quiz";
 import NavBar from "./components/NavBar";
 import HomePage from "./components/HomePage";
+import Quiz from "./components/Quiz";
 
 function App() {
   return (
     <>
-      <NavBar/>
-      <HomePage/>
+      {/* <NavBar/>
+      <HomePage/> */}
+      <Quiz/>
+      
+     
     </>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,7 +7,7 @@ import { VscBook } from "react-icons/vsc";
 import '../Style/Style.css'
 const NavBar = () => {
   return (
-    <div className="Navbar flex justify-between items-center px-10 py-5 bg-[#FFF7F4]">
+    <div className="Navbar flex justify-between items-center px-10 py-5 fixed w-[100%] bg-[#FFF7F4]">
       <div className="Navlogo" style={{color:"#FF5300"}}>
         <h2 className=" text-3xl font-medium">Quiz<span className="text-black"> time</span></h2>
       </div>
@@ -19,7 +19,7 @@ const NavBar = () => {
 
             <a  className="nav_icons"><div  className=" Nav_icon_1"><IoPerson /></div>About</a>
 
-            <a  className="nav_icons"><div  className=" Nav_icon_1"><VscBook /></div>Ouies</a>
+            <a  className="nav_icons"><div  className=" Nav_icon_1"><VscBook /></div>Quiz</a>
 
             <a  className="nav_icons"><div className=" Nav_icon_1"><FaHandPointRight /></div>Practise</a>
           </li>
