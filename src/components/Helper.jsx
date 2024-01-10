e49090c512e4b2e90b1816e7810bf2234812fecb
ashishbahar <ashishbahar987@gmail.com>
2024-01-10 12:40:47 +0530

    code marge

---
+++ src/components/Helper.jsx
@@ -0,0 +1,23 @@
+import adipurush from "../assets/images/png/Adipurush201.png";
+export const QuizData = [
+  {
+    Img: adipurush,
+    Heading: "Sunday Quiz ",
+    Para: "Events from around the world, and from out of this world. Questions to bite into, chow down and ruminate",
+  },
+  {
+    Img: adipurush,
+    Heading: "Sunday Quiz ",
+    Para: "Events from around the world, and from out of this world. Questions to bite into, chow down and ruminate",
+  },
+  {
+    Img: adipurush,
+    Heading: "Sunday Quiz ",
+    Para: "Events from around the world, and from out of this world. Questions to bite into, chow down and ruminate",
+  },
+  {
+    Img: adipurush,
+    Heading: "Sunday Quiz ",
+    Para: "Events from around the world, and from out of this world. Questions to bite into, chow down and ruminate",
+  },
+];
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,12 +1,36 @@
 import React from "react";
+import { QuizData } from "./Helper";
 
 const Quiz = () => {
   return (
-    <>
-      <div>
-        <h2 className=" bg-black ">QUIZ TIME</h2>
+    <div>
+      <div className="max-w-[1140px] mx-auto pt-[40px]">
+        <h2 className=" text-3xl font-bold text-center">QUIZ TIME</h2>
+        <p className="text-center text-2xl mb-[80px] text-black opacity-[.7] mt-2 max-w-[600px] mx-auto">
+          Exercise your brain with these interesting quizzes and puzzles; win
+          points, badges, prizes and impress your friends & colleagues!
+        </p>
+
+        <p className="font-medium text-3xl ">Latest Quizzes</p>
+        <div className=" flex-wrap pt-10 mx-[-12px] flex-col lg:flex-row flex">
+          {QuizData &&
+            QuizData.map((obj, i) => {
+              return (
+                <div className="w-[25%] px-[12px]" key={i}>
+                  <div className="border-solid border-[2px] rounded-lg border-[black] p-[12px]">
+                    <img className=" rounded-lg" src={obj.Img} alt="img" />
+                    <h5 className=" font-bold mt-4 text-xl">{obj.Heading}</h5>
+                    <p className=" pt-1">{obj.Para}</p>
+                    <button className="w-full text-[#fff] bg-black hover:bg-white hover:text-[#000]  duration-200 font-semibold py-2 mt-5 border-[2px] rounded-lg border-solid border-black">
+                      Play
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
